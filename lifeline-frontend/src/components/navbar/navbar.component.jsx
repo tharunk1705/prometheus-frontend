a4b6c4db50419751616f9b87c4f14980124c7450
Tharun Kumar <tharunk1705@gmail.com>
2022-06-17 08:02:37 +0530

    Added Icons

--- lifeline-frontend/src/components/navbar/navbar.component.jsx
+++ lifeline-frontend/src/components/navbar/navbar.component.jsx
@@ -7,36 +7,120 @@ const NAV_ITEMS = [
     route: "/",
     displayText: "Home",
     isAuthRequired: false,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+      </svg>
+    ),
   },
   {
     route: "/resources",
     displayText: "Resources",
     isAuthRequired: false,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+      </svg>
+    ),
   },
   {
     route: "/user/search",
     displayText: "Search donor",
     isAuthRequired: true,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
   },
   {
     route: "/admin/dashboard",
     displayText: "Dashboard",
     isAuthRequired: true,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M2 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H3a1 1 0 01-1-1V4zM8 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H9a1 1 0 01-1-1V4zM15 3a1 1 0 00-1 1v12a1 1 0 001 1h2a1 1 0 001-1V4a1 1 0 00-1-1h-2z" />
+      </svg>
+    ),
   },
   {
     route: "/signup",
     displayText: "Sign up",
     isAuthRequired: false,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
+      </svg>
+    ),
   },
   {
     route: "/signin",
     displayText: "Sign in",
     isAuthRequired: false,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
   },
   {
     route: "/signout",
     displayText: "Sign out",
     isAuthRequired: false,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 inline-block ml-1 mb-1"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+        />
+      </svg>
+    ),
   },
 ];
 
@@ -81,7 +165,9 @@ const Navbar = () => {
         {NAV_ITEMS.map((item, index) => {
           return (
             <li key={item.displayText + index} className="navbar-list-item">
-              <Link to={item.route}>{item.displayText}</Link>
+              <Link to={item.route}>
+                {item.displayText} {item.icon}
+              </Link>
             </li>
           );
         })}
