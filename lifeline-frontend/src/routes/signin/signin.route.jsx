9b408ac5f1acbc8a638e1e40bc2959a667e219f9
Tharun Kumar <tharunk1705@gmail.com>
2022-06-20 22:26:48 +0530

    Home page initial setup

--- lifeline-frontend/src/routes/signin/signin.route.jsx
+++ lifeline-frontend/src/routes/signin/signin.route.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -6,12 +6,33 @@ const SignIn = () => {
   const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
+  useEffect(() => {
+    if (email.includes("@") && email.endsWith(".com")) {
+      setEmailError("");
+    } else {
+      setEmailError("Enter a valid email address");
+    }
+  }, [email]);
+
+  const onChangeHandler = (event) => {
+    switch (event.target.name) {
+      case "email":
+        setEmail(event.target.value);
+        break;
+      case "password":
+        setPassword(event.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div className="pt-16 flex justify-center items-center">
+    <div className="md:pt-16 flex justify-center items-center h-full">
       <div className=" p-8 md:p-10 w-full md:w-1/3 border border-accent-50 rounded-lg">
         <h1 className="text-2xl font-semibold font-Outfit text-center">
           Signin
@@ -26,10 +47,14 @@ const SignIn = () => {
             </label>
             <input
               type="email"
+              name="email"
+              title="Enter your email address"
+              placeholder="Enter your email address"
               required
               className=" rounded-sm p-2 font-medium focus:outline-none focus:ring-2 ring-accent-100 bg-gray-100"
+              onChange={onChangeHandler}
             />
-            {emailError.length > 0 && (
+            {email && emailError.length > 0 && (
               <span className="text-sm text-accent-300 my-2">{emailError}</span>
             )}
           </div>
@@ -40,8 +65,12 @@ const SignIn = () => {
 
             <input
               type="password"
+              name="password"
+              title="Enter your password"
+              placeholder="Enter your password"
               required
               className=" rounded-sm p-2 font-medium focus:outline-none focus:ring-2 ring-accent-100 bg-gray-100"
+              onChange={onChangeHandler}
             />
             {passwordError.length > 0 && (
               <span className="text-sm text-accent-300 my-2">
@@ -50,11 +79,17 @@ const SignIn = () => {
             )}
           </div>
 
-          <div className="flex flex-col gap-8 ">
-            <button className="bg-gray-100 border-gray-400 border-2 rounded-lg py-2 mt-4">
+          <div className="flex flex-col gap-8 font-Outfit ">
+            <button
+              type="reset"
+              className="bg-gray-100 border-gray-400 border-2 rounded-lg py-2 mt-4 "
+            >
               Cancel
             </button>
-            <button className="bg-accent-100 text-white shadow-lg shadow-gray-300   rounded-lg py-2">
+            <button
+              type="submit"
+              className="bg-accent-100 text-white shadow-lg shadow-gray-300   rounded-lg py-2"
+            >
               Submit
             </button>
           </div>
